71621d893803d8bbdc4b3d65898c93dd00789c33
basyair7 <basyair.fathul@gmail.com>
2024-04-11 17:17:35 +0700

    first commit program

--- latihan01.c
+++ latihan01.c
@@ -6,16 +6,16 @@
 #include <stdio.h>
 
 int main() {
-// diketahui v = 150 km/jam; t = 120 detik
-int v = 150,  t = 120;
+    // diketahui v = 150 km/jam; t = 120 detik
+    int v = 150,  t = 120;
 
-// ubah detik ke jam
-// t = 120 detik = 2 menit = 2/60
-t = 2;
-int s = v*t/60;
+    // ubah detik ke jam
+    // t = 120 detik = 2 menit = 2/60
+    t = 2;
+    int s = v*t/60;
 
-// tampilkan hasilnya
-printf("Jadi, panjang lintasan mobil adalah %d km", s);
+    // tampilkan hasilnya
+    printf("Jadi, panjang lintasan mobil adalah %d km", s);
 
-return 0;
+    return 0;
 }
